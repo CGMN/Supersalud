ab0b5ab1f643ec2113634755ab5140e9a293eb2e
CGMN <hadesis@gmail.com>
2019-01-09 16:20:55 -0300

    version con nueva pagina

--- super_selenium2.py
+++ super_selenium2.py
@@ -6,7 +6,6 @@ import csv
 import traceback
 import pandas as pd
 import numpy as np
-import tkinter.filedialog, re
 import time
 
 starttime =  time.time()
@@ -19,20 +18,20 @@ print('Leyendo archivo de Ruts a consultar')
 df = pd.read_csv("Ruts_a_consultar.csv", encoding="latin1", low_memory = False)
 
 print('Creando lista de Rut')
-#RUT=[]
-#for i in df['RUT_FUNCIONARIO']:
-#   RUT.append(str(i))
+RUT=[]
+for i in df['RUT_FUNCIONARIO']:
+   RUT.append(str(i))
 
-RUT=["6249403","7335462","15490509","5509946","12486951","7813596","15745523","2637823","1616681"]
+#RUT=["16609710","14285470","6249403","7335462","15490509","5509946","12486951","7813596","15745523",
+#    "2637823","1616681","13450412","17028841"]
 
-print('Leyendo las especialidades')
-df = pd.read_csv("Especialidades.csv", encoding="latin1", low_memory = False)
-
-print('Creando listas de especialidades')
-especialidades=[]
-for i in df['Especialidades']:
-   especialidades.append(str(i))
-
+#6249403-4
+#7335462-5
+#1616681-2
+#13450412-9
+#16609710-K
+#17028841-6
+#14285470-8 tec med - med
 
 #ignorando errores de certificacion
 options = webdriver.ChromeOptions()
@@ -40,24 +39,22 @@ options.add_argument('--ignore-certificate-errors')
 options.add_argument('--ignore-ssl-errors')
 
 browser = webdriver.Chrome(chrome_options=options)
-
+browser.set_window_size(780, 520)
 
 print (len(RUT), 'Ruts a consultar')
-resultados=[['RUT','Titulo','Parrafo especialidad','Especialidad']]
-
+resultados=[['RUT',"Profesion","Espe"]]
 
 print('Realizando las consultas a la pagina web de la SuperIntendencia de salud')
-
-for i in range(0,len(RUT)):
+for j in range(0,len(RUT)):
 
     try:#___________________________________________________________________________________
         #Paso 1 entrar con el RUT
 
         #la direccion la obtuve de Ctrl+Mayus+I, luego Network y Headers, ahi se busca entre los movimientos
-        browser.get("http://webhosting.superdesalud.gob.cl/bases/prestadoresindividuales.nsf/(searchAll2)/Search?SearchView&Query=(FIELD%20rut_pres="+RUT[i]+")&Start=1&count=10")
+        browser.get("http://webhosting.superdesalud.gob.cl/bases/prestadoresindividuales.nsf/(searchAll2)/Search?SearchView&Query=(FIELD%20rut_pres="+RUT[j]+")&Start=1&count=10")
         browser.implicitly_wait(0)
-        #con esto tengo el nombre, para obtener la clase usé:
 
+        #con esto tengo el nombre, para obtener la clase usé:
         #r = requests.get("http://webhosting.superdesalud.gob.cl/bases/prestadoresindividuales.nsf/(searchAll2)/Search?SearchView&Query=(FIELD%20rut_pres=6249403)&Start=1&count=10")
         #print(r.text) ahi sale la clase
 
@@ -65,54 +62,111 @@ for i in range(0,len(RUT)):
         browser.implicitly_wait(0)
         #___________________________________________________________________________________
         #Paso 2 click en el NOMBRE
-
         elm=browser.find_element_by_link_text(str(nombre))
-        browser.implicitly_wait(5)
+        browser.implicitly_wait(0)
         elm.click()
 
-        #___________________________________________________________________________________
-        #Paso 3 capturar la url actual
+        #para saber la cantidad de filas de la tabla_______________________________________________________________________________
+        primer_cuadro=browser.find_elements_by_xpath("//table[@class = 'antecedente']/tbody/tr")
 
+        profesion=[]
+        profesion.append(browser.find_element_by_xpath('//*[@id="ficha"]/div/table/tbody/tr[2]/td/table/tbody/tr[1]/td[1]').text)
+        profesion_limpio=','.join(profesion)
+        profesion_limpio=profesion_limpio.replace("Título o habilitación profesional : ","")
+
+
+        espec_lista=[]
+        for i in range(2,len(primer_cuadro)+1):
+            espec_lista.append(browser.find_element_by_xpath('//*[@id="ficha"]/div/table/tbody/tr[2]/td/table/tbody/tr['+str(i)+']/td[1]').text)
+
+        espec_lista=espec_lista[::-1] #invierte el orden
+
+        espec_lista_limpia=[]
+        for i in range(0, len(espec_lista)):
+            b=espec_lista[i].replace("Especialidad : ","") #quita la palabra especialidad
+            espec_lista_limpia.append(b)
+
+        #obtener las fechas desde el bloque de texto
+
+        #Paso 3 capturar la url actual
         url=browser.current_url
         #print(url)
 
         #___________________________________________________________________________________
         #Paso 4 seleccionar la clave para buscar el certificado
-
         slice_str=url[85:117]
-        #print(slice_str)
 
         #Paso 5 ingresar a la pagina del certificado
         browser.get('http://webhosting.superdesalud.gob.cl/bases/prestadoresindividuales.nsf/CertificadoRegistro?openform&pid='+str(slice_str))
 
+        especialidad=browser.find_element_by_xpath("/html/body/form/table/tbody/tr[2]/td/div/table/tbody/tr[13]/td").text
+        espe=especialidad.replace("\n"," ")
+        espe=espe.replace('°','')
+        espe=espe.replace('"','')
+        espe=espe.replace('“','')
+        espe=espe.replace('     ','')
 
-        #Paso 6 Extraer la información  Ctrl+Mayus+I con eso puedo buscar los elementos, los copié como xpath
-        rut=browser.find_element_by_xpath("/html/body/form/table/tbody/tr[2]/td/div/table/tbody/tr[6]/td[2]").text
-        rut=rut.replace(".","")
-        rut=rut.replace("-","")
-        rut_corto_sin_p_g=rut[0:-1]
+        #cambiar las fechas
+        meses_dic = {" de Enero de ":"/01/", " de Febrero de ":"/02/"," de Marzo de ":"/03/"," de Abril de ":"/04/",
+            " de Mayo de ":"/05/", " de Junio de ":"/06/"," de Julio de ":"/07/"," de Agosto de ":"/08/",
+            " de Septiembre de ":"/09/", " de Octubre de ":"/10/", " de Noviembre de ":"/11/", " de Diciembre de ":"/12/"}
+        dias={' 1/':' 01/',' 2/':' 02/',' 3/':' 03/',' 4/':' 04/',' 5/':' 05/',' 6/':' 06/',' 7/':' 07/',' 8/':' 08/',' 9/':' 09/'}
 
-        titulo=browser.find_element_by_xpath("/html/body/form/table/tbody/tr[2]/td/div/table/tbody/tr[11]/td[2]/b").text
+        for n, m in meses_dic.items(): #cambiamos las fechas al hacer un loop en el diccionario
+            espe = espe.replace(n, m)
 
+        for p, t in dias.items(): #cambiamos las fechas al hacer un loop en el diccionario cuando son menores a 10
+            espe = espe.replace(p, t)
 
+        #determinar las posiciones de las fechas
+        fechas_estandar=["/01/","/02/","/03/","/04/","/05/","/06/","/07/","/08/","/09/","/10/","/11/","/12/"]
 
+        import re
+        #lista con la posición de las fechas
+        pos_fechas=[]   #esta mal el orden
+        for r in range(0,len(fechas_estandar)):
+            for m in re.finditer(fechas_estandar[r], espe):
+                pos_fechas.append(m.start())
+        pos_fecha_prof=pos_fechas[0]
+        pos_fechas.pop(0)
 
-        resultados.append([rut_corto_sin_p_g,titulo])
+        #lista con las fechas
+        fechas=[]  #esta mal el orden
+        for i in range(0, len(pos_fechas)):
+            fechas.append(espe[(pos_fechas[i]-2):(pos_fechas[i]+8)])
+
+        #tupla de fecha y posicion para ordenarla
+        pareo_pos_fecha=[]
+        for d in range(0, len(fechas)):
+            pareo_pos_fecha.append((pos_fechas[d],fechas[d]))
+
+        orden_pareo=sorted(pareo_pos_fecha) #orden de el pareo entre posicion y fechas
+
+        #lista con las fechas ordenadas obtenidas del pareo
+        fechas_ordenadas=[]
+        for i in range(0,len(orden_pareo)):
+            fechas_ordenadas.append(orden_pareo[i][-1])
+
+        #unir las fechas con las Especialidades
+        espec_con_fecha=[]
+        for i in range(0, len(fechas_ordenadas)):
+            espec_con_fecha.append([espec_lista_limpia[i],fechas_ordenadas[i]])
+
+        espec_con_fecha_junto=[]
+        for i in range(0,len(espec_con_fecha)):
+            an=';'.join(espec_con_fecha[i])
+            espec_con_fecha_junto.append(an)
+        print (espec_con_fecha_junto)
+
+        resultados.append([RUT[j],profesion_limpio,espe,fechas_ordenadas,espec_lista_limpia,espec_con_fecha_junto])
 
     except:
         var = traceback.format_exc()
-        resultados.append([RUT[i],"0","0"])
+        resultados.append([RUT[j],"0","0","0","0"])
         #print (var)
 if var !=0:
     f.write(var)
     f.close()
-    #print ("")
-    #print (" X X X X X X X X X X X X X X X X X X ")
-    #print ("")
-    #print ("El programa arroja un error")
-    #print ("Se ha creado un archivo llamado 'Archivo_de_error' con el detalle")
-    #print ("")
-    #print (" X X X X X X X X X X X X X X X X X X ")
 
 f.close()
 
@@ -121,13 +175,15 @@ browser.close()
 print ("cantidad de funcionarios consultados:", len(resultados)-1)
 
 #___________________________________________________________________________________
-
 with open("output.csv", "w",newline='') as f:
     writer = csv.writer(f)
     writer.writerows(resultados)
 
-print ("tiempo de ejecucion:", int(time.time()-starttime)," segundos")
-print("terminado")
+print ("")
+print ("Tiempo de ejecucion:", int(time.time()-starttime)," segundos")
+print ("Ejecutado a las:",time.strftime("%H:%M"))
+print ("")
+print ("Proceso Terminado")
 
 
 #'http://webhosting.superdesalud.gob.cl/bases/prestadoresindividuales.nsf/(searchAll2)/F14E03A2D6E3930A8425763C006A2A51?OpenDocument'
---
+++ super_selenium3.py
@@ -0,0 +1,300 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import traceback
+import csv
+import pandas as pd
+import numpy as np
+import time
+import pyperclip
+
+#ctrl+/ para comentarios
+
+starttime =  time.time()
+
+f= open("Archivo_de_error.txt","w")
+
+var=0
+#ignorando errores de certificacion
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+
+browser = webdriver.Chrome(chrome_options=options)
+browser.set_window_size(980, 720)
+
+#rut=["1.616.681-2"]
+#rut=["14.285.470-8"]
+#rut=["13.450.412-9"]
+
+rut=["6.249.403-4","15.490.509-K","1.616.681-2","14.285.470-8","15.312.391-8",
+    "15.341.395-9","14.048.954-9","8.744.345-0","24.050.211-9"]
+
+print('\nLeyendo archivo de Ruts a consultar')
+df = pd.read_csv("Ruts_a_consultar.csv", encoding="latin1", low_memory = False)
+
+print('\nCreando lista de Rut')
+
+# rut=[]
+# for i in df['RUT_FUNCIONARIO']:
+#   rut.append(str(i))
+
+print("")
+print (len(rut), 'Ruts a consultar')
+
+espera=1
+
+#Funcion que cambia de str a list
+def texto_a_lista(texto):
+    out = []
+    buff = []
+    for c in texto:
+        if c == '\n':
+            out.append(''.join(buff))
+            buff = []
+        else:
+            buff.append(c)
+    else:
+        if buff:
+           out.append(''.join(buff))
+    return out
+
+resultados=[['RUT',"Nacionalidad","nuevo_textocompleto","Eunacom","N de Profesiones","Profesion","N de Especialidades","Especialidades"]]
+
+print("\nConectandose a la pagina de la SuperIntendencia de Salud")
+
+from progress.bar import Bar
+
+print("")
+#bar = Bar('Procesando', fill="*",max=len(rut))
+
+for n in range(0, len(rut)):
+
+    try:
+
+        #<editor-fold #Conexion
+
+        browser.implicitly_wait(espera)
+        browser.get("https://rnpi.superdesalud.gob.cl/?openForm")
+
+        browser.implicitly_wait(espera)
+        ingresar_rut=browser.find_element_by_xpath("/html/body/div[1]/div/div/div[3]/form/div/input")#la cajita donde se pone el rut
+        ingresar_rut.send_keys(rut[n])
+
+        browser.implicitly_wait(espera)
+        buscar=browser.find_element_by_xpath("/html/body/div[1]/div/div/div[3]/form/div/span/button")#el boton de buscar
+        buscar.send_keys('\n')  #como si fuera un click
+        #14.285.470-8 mas de una profesion
+        #6.249.403-4
+        #7.335.462-5
+        #1.616.681-2  mas de una especialidad
+        #13.450.412-9
+        #16.609.710-K
+        #17.028.841-6
+
+        #</editor-fold>
+
+
+        #<editor-fold # Profesion
+
+        #profesion
+        browser.implicitly_wait(espera)
+        prof=browser.find_element_by_css_selector("#main > div > div.fill.ng-scope > div > table > tbody > tr > td:nth-child(5)").text
+        profesion=texto_a_lista(prof)
+
+        pos_eunacom=[i for i,x in enumerate(profesion) if x=='(EUNACOM)']
+
+        #si eunacom es >0
+        prof_eunacom=[]
+        if len(pos_eunacom)>0 and len(profesion)==2:
+            prof_eunacom.append(profesion[pos_eunacom[0]-1])
+        nuevo_prof_eunacom=','.join(prof_eunacom)
+
+        if len(pos_eunacom)>0:
+            profesion.pop(pos_eunacom[0])
+            profesion.pop((pos_eunacom[0]-1))
+
+        prof_limpia=[]
+        for i in range(0,len(profesion)):
+            if i%2==0:
+                prof_limpia.append(profesion[i])
+
+
+        #</editor-fold>#
+
+
+        #<editor-fold #especialidades
+        #especialidad
+        browser.implicitly_wait(espera)
+        espe=browser.find_element_by_css_selector("#main > div > div.fill.ng-scope > div > table > tbody > tr > td:nth-child(6)").text
+        especialidades=texto_a_lista(espe)
+        espe_limpia=[]
+        for i in range(0,len(especialidades)):
+            if i%2==0:
+                espe_limpia.append(especialidades[i])
+
+        #print (espe_limpia)
+        #</editor-fold>#
+
+
+        #<editor-fold # copiar texto del certificado
+
+        browser.implicitly_wait(espera)
+        bb=browser.find_element_by_css_selector("#main > div > div.fill.ng-scope > div > table > tbody > tr > td:nth-child(2) > h4 > a") #para abrir el certificado
+        bb.send_keys('\n')
+
+        #Esperar texto visible para copiar_______________________________________
+        from selenium.webdriver.support import expected_conditions as EC
+
+        wait = WebDriverWait(browser, 10)
+        element = wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#main > div > div.fill.ng-scope > div > div > div.x_panel.col-12 > div > div:nth-child(2) > table > tbody > tr:nth-child(2) > td'),"Posee"))
+        #________________________________________________________________________
+
+        #copio el texto de la pagina web con el teclado
+        p2=browser.find_element_by_css_selector("body")
+        #time.sleep(0.5)
+        from ctypes import windll
+        if windll.user32.OpenClipboard(None):
+            windll.user32.EmptyClipboard()
+            windll.user32.CloseClipboard()
+        p2.send_keys(Keys.CONTROL+'a')
+        p2.send_keys(Keys.CONTROL+'c')
+        textocompleto=pyperclip.paste()
+        textocompleto=textocompleto.replace('\r','')
+        #print(textocompleto)
+
+        #para separar el str en una lista
+        nuevo_textocompleto=texto_a_lista(textocompleto)
+
+        nuevo_textocompleto.pop(0) #quito el primer elemento por ser vacio
+        #print (nuevo_textocompleto)
+        nuevo_textocompleto2=','.join(nuevo_textocompleto)
+
+        #</editor-fold>#
+
+
+        #<editor-fold # depuracion de profesiones y fechas
+
+        #posiciones de las profesiones en el texto grande para poder encontrar las fechas
+        anterior_pos_prof=[i for i,x in enumerate(nuevo_textocompleto) if x=='Antecedente del Título o habilitación profesional']
+
+        examen_en_texto = [s for s in nuevo_textocompleto if "Examen Único Nacional de Conocimientos de Medicina" in s]
+
+        pos_texto_examen=[]
+        if len(examen_en_texto)>0:
+            for i in range(0,len(examen_en_texto)):
+                pos_texto_examen.append(nuevo_textocompleto.index(examen_en_texto[i]))
+
+        import re
+        from datetime import datetime
+
+        fecha_eunacom=[]
+        if len(examen_en_texto)>0:
+            for i in range(0,len(pos_texto_examen)):
+                text = nuevo_textocompleto[pos_texto_examen[i]]
+                match = re.search(r'\d{2}-\d{2}-\d{4}', text)
+                date = datetime.strptime(match.group(), '%d-%m-%Y').date()
+                fecha_eunacom.append(str(date))
+        else:
+            fecha_eunacom.append("0")
+        fecha_eunacom2=','.join(fecha_eunacom)
+
+        #quitando de la pos anterior prof el eunacom
+        if len(pos_texto_examen)>0:
+            o=anterior_pos_prof.index(pos_texto_examen[0]-1)
+            anterior_pos_prof.pop(o)
+
+        pos_prof=[]
+        for i in range(0,len(anterior_pos_prof)):
+            pos_prof.append((anterior_pos_prof[i]+1))
+
+        fechas_prof=[]
+
+        for i in range(0,len(pos_prof)):
+            text = nuevo_textocompleto[pos_prof[i]]
+            match = re.search(r'\d{2}-\d{2}-\d{4}', text)
+            date = datetime.strptime(match.group(), '%d-%m-%Y').date()
+            fechas_prof.append(str(date))
+
+
+        prof_fecha=[]
+        for i in range(0, len(prof_limpia)):
+            prof_fecha.append((str(prof_limpia[i])+";"+str(fechas_prof[i])))
+
+        nueva_prof_fecha=','.join(prof_fecha)
+
+        #</editor-fold>#
+
+        #me falta el año cuando son titulos extranjeros
+
+        #<editor-fold #depuracion de especialidades y fechas
+
+        anterior_pos_espe=[i for i,x in enumerate(nuevo_textocompleto) if x=='Antecedente de la Especialidad']
+
+
+        pos_espe=[]
+        for l in range(0,len(anterior_pos_espe)):
+            pos_espe.append((anterior_pos_espe[l]+1))
+
+
+        fechas_espe=[]
+
+        for i in range(0,len(pos_espe)):
+            text = nuevo_textocompleto[pos_espe[i]]
+            match = re.search(r'\d{2}-\d{2}-\d{4}', text)
+            date = datetime.strptime(match.group(), '%d-%m-%Y').date()
+            fechas_espe.append(str(date))
+
+
+        espe_fecha=[]
+        for i in range(0, len(espe_limpia)):
+            espe_fecha.append((str(espe_limpia[i])+";"+str(fechas_espe[i])))
+        espe_fecha2=','.join(espe_fecha)
+
+        #</editor-fold>#
+
+
+        #<editor-fold #Nacionalidad
+
+        browser.implicitly_wait(espera)
+        nacion=browser.find_element_by_css_selector("#main > div > div.fill.ng-scope > div > div > div.x_panel.col-12 > div > table > tbody > tr:nth-child(7) > td.ng-binding").text
+
+        #</editor-fold>#
+
+        if len(prof_eunacom)>0:
+            resultados.append([rut[n],nacion,nuevo_textocompleto2,fecha_eunacom2,
+                len(prof_limpia),nuevo_prof_eunacom,len(espe_limpia),espe_fecha2])
+        else:
+            resultados.append([rut[n],nacion,nuevo_textocompleto2,fecha_eunacom2,
+                len(prof_limpia),nueva_prof_fecha,len(espe_limpia),espe_fecha2])
+        #bar.next()
+
+    except:
+
+        var = traceback.format_exc()
+        resultados.append([rut[n],"0","0","0","0","0","0","0"])
+        #bar.next()
+#bar.finish()
+
+
+
+if var !=0:
+    f.write(var)
+    f.close()
+f.close()
+
+browser.close()
+#print ("cantidad de funcionarios consultados:", len(resultados)-1)
+
+#escribir el archivo
+print("\nEscribiendo el archivo")
+with open("output.csv", "w",newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(resultados)
+print("\nArchivo listo")
+
+print ("\nTiempo de ejecucion:", int(time.time()-starttime)," segundos")
+print ("Ejecutado a las:",time.strftime("%H:%M"),"\n")
+print ("Proceso Terminado")
